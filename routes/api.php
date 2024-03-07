54cfeea5478f027ed576b3e748bbae7bf93106bf
Adrián Villa <idgs.adrian.villa@gmail.com>
2024-03-07 10:54:04 -0600

    Corrección rutas

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/comporbantesPago/capturaComporbantePago', 'App\Http\Controllers\ComprobantesPagoController@capturaComporbantePago');
-Route::get('/prueba', 'App\Http\Controllers\ComprobantesPagoController@prueba');
+Route::post('/comporbantesPago/capturaComporbantePago', 'App\Http\Controllers\ChatBot\ComprobantesPagoController@capturaComporbantePago');
+Route::get('/prueba', 'App\Http\Controllers\ChatBot\ComprobantesPagoController@prueba');
